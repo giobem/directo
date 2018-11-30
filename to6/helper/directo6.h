fd46a2027b3851dc2732f3b67f040af14b83932e
giobem <31698337+giobem@users.noreply.github.com>
2018-11-30 11:20:02 +0000

    Create helper/directo6.h

---
+++ to6/helper/directo6.h
@@ -0,0 +1,24 @@
+#ifndef __DIRECTO6__
+#define __DIRECTO6__
+
+#define DIRECT_TO_HELPER 8197
+#define DIRECT_TO_KERNEL 8198
+#define DIRECTO6_BUFFER_SIZE 0x10001
+
+#define DIRECT_INCOMING 0
+#define DIRECT_OUTGOING 1
+#define FROM4_INCOMING  2
+#define NO_CODE   127
+#define RELAY_TO6 128
+#define RELAY_TO4 129
+#define GO        253
+#define EXITNOW   254
+
+#define TCPCONF    32
+#define UDPCONF    33
+#define RELAY4CONF 34
+
+#define DR_KEY   0x10
+#define DR_ASKEY 0x11
+
+#endif
