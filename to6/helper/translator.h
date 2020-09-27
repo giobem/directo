49ce531287231ffc0f1fb51012a58bafc9f2e4f6
Giovanni Bembo <giovanni.bembo@students.uniroma2.eu>
2020-09-27 14:38:48 +0200

    Update translator.h

--- to6/helper/translator.h
+++ to6/helper/translator.h
@@ -1,4 +1,12 @@
 #ifndef __DIRECTO6_TRANSLATOR__
 #define __DIRECTO6_TRANSLATOR__
 
+void send_to_fragment(unsigned char *,int);
+void rcv_from_relay(unsigned char *,int);
+void send_to_relay(unsigned char *,int);
+void ipv6_mtu_autored(unsigned char *,int);
+void set_host(char *,char *);
+void set_relay(char *,char *);
+int setsocket(unsigned char *,unsigned char *);
+
 #endif
